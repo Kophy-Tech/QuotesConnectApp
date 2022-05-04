63f5ad233bc64a3a201f8f9a65189d635b4dcdb7
omidiora <omidioraemmanuel@gmail.com>
2022-05-04 21:40:40 +0100

    more changes may 4 9:40pm

--- src/Pages/Splash.js
+++ src/Pages/Splash.js
@@ -1,22 +1,18 @@
 //import liraries
 import React, {Component, useEffect} from 'react';
 import {View, Text, StyleSheet, Image, ActivityIndicator} from 'react-native';
+import AppBar from '../component/AppBar';
 import {COLOR, WP, HP, IMAGE, FONT, TEXT_SIZES, APP_NAME} from '../Utils/theme';
 
 const Splash = ({navigation}) => {
   return (
     <View style={styles.container}>
+        <AppBar backgroundColor={COLOR.whiteColor} type={"dark-content"}/>
       <Image
         source={IMAGE.splash_icon}
         style={{width: WP('52'), height: HP('12')}}
         resizeMode="cover"
       />
-
-      {/* <View
-                style={{ position: 'absolute', bottom: HP('10') }}     >
-                <ActivityIndicator size="large" color={COLOR.primary} />
-
-            </View> */}
     </View>
   );
 };
