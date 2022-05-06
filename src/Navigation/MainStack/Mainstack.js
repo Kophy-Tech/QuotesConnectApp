489d5262de87b09159a7555ae8b5c2a7ae038392
Temitope <ojotemitopeidnoble10@gmail.com>
2022-05-06 15:37:57 -0700

    update

--- src/Navigation/MainStack/Mainstack.js
+++ src/Navigation/MainStack/Mainstack.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-<<<<<<< HEAD
 import AuthStack from '../AuthStack/AuthStack';
 import BottomStack from '../BottomStack/BottomStack';
 
-=======
->>>>>>> 4010c39c21bfaffe81a279ae091934f60daa21ee
 import Splash from '../../Pages/Splash';
 import Onboarding from '../../Pages/Onboarding';
 import AuthStack from '../AuthStack/AuthStack';
-import Login from '../../Pages/Auth/Login';
+
 
 const Stack = createStackNavigator();
 
@@ -20,13 +17,13 @@ function MainStack() {
       screenOptions={{
         headerShown: false,
       }}>
-      {/* <Stack.Screen name="Splash" component={Splash} /> */}
-      {/* <Stack.Screen name="Auth" component={AuthStack} /> */}
-      <Stack.Screen name="bottomStack" component={BottomStack} />
+    
 
       <Stack.Screen name="Splash" component={Splash} />
       <Stack.Screen name="Onboarding" component={Onboarding} />
       <Stack.Screen name="Auth" component={AuthStack} />
+      <Stack.Screen name="bottomStack" component={BottomStack} />
+
     </Stack.Navigator>
   );
 }
